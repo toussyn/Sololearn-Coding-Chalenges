044fb9465cd83fdf26325b5dde9246816302dd53
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-03-06 16:43:17 +0100

    Create Loading.js

---
+++ JavaScript/Loading.js
@@ -0,0 +1,17 @@
+/*
+User-Defined Functions
+
+
+It’s very common to have "Preloader" component, especially in multifunctional apps and websites.
+Create a function that will output "Loading" to the console.
+
+Output
+Loading
+*/
+
+function showPreloader() {
+    console.log("Loading")
+
+}
+
+showPreloader();
