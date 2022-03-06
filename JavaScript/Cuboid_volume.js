a4a86ad8b92cce8df3b5fb3461571b384990708f
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-03-06 16:57:41 +0100

    Create Cuboid_volume.js

---
+++ JavaScript/Cuboid_volume.js
@@ -0,0 +1,17 @@
+/*
+Introducing Objects
+
+
+The given class represents a cuboid (e.g. a rectangular prism) that contains the properties of length, width, and height.
+Complete the program to calculate and output the volume of given cuboid to the console.
+*/
+
+var cuboid = {
+    length: 25,
+    width: 50,
+    height: 200
+};
+
+let volume = cuboid.length * cuboid.width * cuboid.height;
+
+console.log(volume);
