21c834452766e27ee2cb9b625d56dca56531e4a5
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-03-06 16:11:31 +0100

    Create Vacation_month.js

---
+++ JavaScript/Vacation_month.js
@@ -0,0 +1,20 @@
+/*
+You are planning a vacation in August.
+You are given a program that takes the month as input.
+
+Task
+Complete the code to output "vacation", if the given month is August. Don't output anything otherwise.
+
+Sample Input
+August
+
+Sample Output
+vacation
+Handle the required condition using an if statement.
+Use console.log() for the output.
+*/
+
+var month = readLine()
+if (month === "August"){
+    console.log("vacation")
+}
