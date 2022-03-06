d51ff125d550a2f783c6dada827bf25b756695a2
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-03-06 15:34:31 +0100

    Create Equal_Numbers.js

---
+++ JavaScript/Equal_Numbers.js
@@ -0,0 +1,17 @@
+/*
+The first two lines of the given code take two numbers as input.
+
+Task
+Complete the code to output whether or not the numbers are equal (true or false).
+
+Sample Input
+11
+11
+
+Sample Output
+true
+*/
+
+var num1 = parseInt(readLine(), 10);
+var num2 = parseInt(readLine(), 10);
+  console.log(num1 === num2); 
