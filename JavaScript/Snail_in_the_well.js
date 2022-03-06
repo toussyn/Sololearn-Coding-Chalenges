0e3db2b5a5e04d8ce884a47effcc47cfcf7052d6
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-03-06 16:41:17 +0100

    Create Snail_in_the_well.js

---
+++ JavaScript/Snail_in_the_well.js
@@ -0,0 +1,32 @@
+/*
+The snail climbs up 7 feet each day and slips back 2 feet each night.
+How many days will it take the snail to get out of a well with the given depth?
+
+Sample Input:
+31
+
+Sample Output:
+6
+
+Explanation: Let's break down the distance the snail covers each day:
+Day 1: 7-2=5
+Day 2: 5+7-2=10
+Day 3: 10+7-2=15
+Day 4: 15+7-2=20
+Day 5: 20+7-2=25
+Day 6: 25+7=32
+So, on Day 6 the snail will reach 32 feet and get out of the well at day, without slipping back that night.
+*/
+
+function main() {
+    var depth = parseInt(readLine(), 10);
+     var day = 1; 
+     while (depth > 0) {
+          depth -= 7; 
+          if(depth <= 0) { 
+              break; 
+              } 
+              depth += 2; ++day; 
+     } 
+     console.log(day);    
+}
