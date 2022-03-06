26cdb2dc6f36a257eb6d775232a2383b9b1d3b82
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-03-06 15:48:01 +0100

    Create Noon_or_midnight.js

---
+++ JavaScript/Noon_or_midnight.js
@@ -0,0 +1,21 @@
+/*
+Time flies when you’re having fun.
+Given a clock that measures 24 hours in a day, write a program that takes the hour as input. If the hour is in the range of 0 to 12, output am to the console, and output pm if it's not.
+
+Sample Input
+13
+
+Sample Output
+pm
+Assume the input number is positive and less than or equal to 24.
+*/
+
+function main() {
+    var hour = parseInt(readLine(), 10);
+    // Your code goes here
+    if (hour <= 12){
+        console.log("am")
+        } else {
+            console.log("pm")
+            }
+}
