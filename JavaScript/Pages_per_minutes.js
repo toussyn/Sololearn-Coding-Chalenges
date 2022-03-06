27a8bb153febe517b79184f5e5826f7468aa4eb6
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-03-06 16:33:09 +0100

    Create Pages_per_minutes.js

---
+++ JavaScript/Pages_per_minutes.js
@@ -0,0 +1,19 @@
+/*
+The For loop
+
+
+The printer prints 20 pages per minute.
+Write a program that will output to the console how many pages will be printed each minute over the course of 6 minutes.
+
+Output
+20
+40
+60
+80
+100
+120
+*/
+
+for (let x = 20; x <= 120; x += 20){
+    console.log(x)
+    };
