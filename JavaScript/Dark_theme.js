b6d0fa183a7d6fceb06bc0f4ef0e3b2daf575cc8
Adijat Ojutomori <43005376+toussyn@users.noreply.github.com>
2022-03-06 16:28:12 +0100

    Create Dark_theme.js

---
+++ JavaScript/Dark_theme.js
@@ -0,0 +1,40 @@
+/*
+Switch statements
+
+The user can choose the color theme for the browser:
+1. Light
+2. Dark
+3. Nocturne
+4. Terminal
+5. Indigo
+
+You are given a program that takes
+the number as input. Complete the program so that it will output to the console the theme according to input number.
+
+Sample Input
+2
+
+Sample Output
+Dark
+*/
+
+function main() {
+    var themeNumber = parseInt(readLine(), 10)
+ switch(themeNumber){
+     case 1: 
+      console.log("Light");
+      break;
+     case 2: 
+      console.log("Dark");
+      break;
+     case 3: 
+     console.log("Nocturne");
+      break;
+     case 4: 
+     console.log("Terminal");
+      break;
+     case 5: console.log("Indigo");
+     
+ }
+    
+}
